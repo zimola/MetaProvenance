b5d660bf0d9ce9f9d4ac9d9280687f7ca61ae0fe
zimola <matt.zimola@gmail.com>
2018-11-15 10:22:14 -0400

    added langindpage app.

--- metaprovenance/settings.py
+++ metaprovenance/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'accounts',
+    'landingpage',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- metaprovenance/urls.py
+++ metaprovenance/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from landingpage.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('accounts.urls')),
+    path('', index),
 
 ]
