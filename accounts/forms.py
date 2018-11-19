af0020096673a63a0c88420345b5e516171177f5
zimola <matt.zimola@gmail.com>
2018-11-19 14:18:16 -0400

    front-end auth complete

--- accounts/forms.py
+++ accounts/forms.py
@@ -6,8 +6,21 @@ User = get_user_model()
 
 
 class SignUpForm(forms.ModelForm):
-    pass
+    password = forms.CharField(widget=forms.PasswordInput)
+    confirm_password = forms.CharField(label="Confirm Password", widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('email',
+                  'password',
+                  )
+
+    def init(self, *args, **kwargs):
+        super(SignUpForm, self).__init__(*args, **kwargs)
+        self.fields['email'].widget.attrs['placeholder'] = 'Email'
+        self.fields['password'].widget.attrs['placeholder'] = 'Password'
 
 
 class SignInForm(forms.Form):
-    pass
+    email = forms.CharField(label="Email", max_length=40)
+    password = forms.CharField(widget=forms.PasswordInput)
--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,35 @@
 from django.shortcuts import render
+from .forms import SignInForm, SignUpForm
 
 
 def account_signup(request):
-    pass
+    form = SignUpForm(request.POST or None)
+    action = 'SignUp'
+
+    if request.method == 'POST' and form.is_valid():
+        # todo clean password
+        #user = form.save(commit=False)
+        email = form.cleaned_data['email']
+        password = form.cleaned_data['password']
+        # print(email, password)
+        pass
+
+    return render(request, 'accounts/signup.html', context={'form': form, 'action': action})
 
 
 def account_signin(request):
-    pass
+    form = SignInForm(request.POST or None)
+    action = 'SignIn'
+
+    if request.method == 'POST' and form.is_valid():
+        #user = form.save(commit=False)
+        email = form.cleaned_data['email']
+        password = form.cleaned_data['password']
+        # print(email, password)
+        pass
+
+    return render(request, 'accounts/signin.html', context={'form': form, 'action': action})
+
 
 
 def account_logout(request):
