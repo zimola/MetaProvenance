a385e38d9fdf25110f96f6b1135b48b3411e48ac
zimola <matt.zimola@gmail.com>
2018-12-11 13:26:55 -0400

    edited requirements.txt created skeleton for uploading

--- db/urls.py
+++ db/urls.py
@@ -7,4 +7,12 @@ urlpatterns = [
     path('search/', views.search, name='search'),
     path('browse/', views.browse, name='browse'),
     path('upload/', views.UploadView.as_view(), name='upload'),
+    path('add/investigation/', views.add_investigations_view, name='add_investigation'),
+    path('search/investigation/', views.investigation_search, name='search_investigation'),
+    path('add/sample/', views.add_sample_view, name='add_sample'),
+    path('add/wetlab/', views.add_wetlab_view, name='add_wetlab'),
+    path('add/biological_protocol/', views.add_biological_protocol_view, name='add_biological_protocol'),
+    path('add/pipeline_results/', views.add_pipeline_results_view, name='add_pipeline_results'),
+
+
 ]
--- db/views.py
+++ db/views.py
@@ -1,13 +1,97 @@
-from django.shortcuts import render
-from .forms import UploadTestForm
+from django.shortcuts import render, redirect
+from .forms import UploadTestForm, CreateInvestigationForm
 from django.http import JsonResponse
 from django.views import View
+from django.contrib.auth.decorators import login_required
+
+#from .models import Investigation
 
 import django_tables2 as tables
 
 from .formatters import format_sample_metadata
 
-import pandas as pd
+#import pandas as pd
+
+
+'''
+INVESTIGATIONS
+'''
+
+
+@login_required()
+def add_investigations_view(request):
+    form = CreateInvestigationForm(request.POST or None)
+    action = "Create Investigation"
+    if request.method == 'POST' and form.is_valid:
+        # todo handle is investigation exists. -> Create or search:
+        investigation = form.save()
+        request.session['investigation_name'] = investigation.name
+        return redirect('landing')
+    context = {'form': form, 'action': action}
+    return render(request, 'db/add_investigation.html', context)
+
+
+def investigation_search(request):
+    # todo full text search on investigation table
+    query = request.GET.get('q')
+    print(query)
+    return redirect('db:add_investigation')
+
+def list_of_investigations():
+    #todo, list of 20 or so most recent investigations
+    pass
+
+
+'''
+SAMPLES
+'''
+
+
+@login_required()
+def add_sample_view(request):
+    # todo make html
+    action = 'Add Samples'
+    context = {'action': action}
+    return render(request, 'db/add_sample.html', context)
+
+
+'''
+WETLAB
+'''
+
+
+@login_required()
+def add_wetlab_view(request):
+    # todo make html
+    action = 'Add Wetlab Data'
+    context = {'action': action}
+    return render(request, 'db/add_wetlab.html', context)
+
+
+'''
+BIOLOGICAL PROTOCOL
+'''
+
+
+@login_required()
+def add_biological_protocol_view(request):
+    #  todo make html
+    action = 'Add Biological Protocol Data'
+    context = {'action': action}
+    return render(request, 'db/add_biological_protocol.html', context)
+
+
+'''
+PIPELINERESULTS
+'''
+
+
+def add_pipeline_results_view(request):
+    # todo make html
+    action = 'Add Pipeline Results'
+    context = {'action': action}
+    return render(request, 'db/add_pipeline_results.html', context)
+
 
 class SampleNameTable(tables.Table):
     selected = tables.columns.CheckBoxColumn(orderable=False,
